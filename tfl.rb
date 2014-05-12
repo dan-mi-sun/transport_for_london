8f7739c5ccded98518e89967fd26b96776812e17
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-12 06:24:11 +0100

    point 1. define array for victoria line

---
+++ tfl.rb
@@ -0,0 +1,16 @@
+class TubeLine
+  
+  attr_accessor :station
+
+  #1. Define suitable data structures for the underground lines and stations.
+  #
+  def initialize
+  @victoria = ["Kings Cross", "Euston", "Warren Street", "Oxford Circus", "Green Park", "Victoria", "Pimlico"]
+  
+  end
+
+end
+
+  #2. Create a text-based program that prompts the user for their starting line, then prints out the stations available to choose from.
+  #
+
