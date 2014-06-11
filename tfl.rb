cde681cd8553ce400caad90835ff690debb217a5
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-11 11:13:23 +0100

    refactor programme to remove unnessecary instance variables

--- tfl.rb
+++ tfl.rb
@@ -23,19 +23,19 @@ end
 class Journey
   
   attr_accessor :list_all_stations 
-  attr_accessor :get_start_line
-  attr_accessor :get_end_line
-  attr_accessor :get_start_station
-  attr_accessor :get_end_station
+  # attr_accessor :get_start_line
+  # attr_accessor :get_end_line
+  # attr_accessor :get_start_station
+  # attr_accessor :get_end_station
 
 
   #2. Create a text-based program that prompts the user for their starting line, then prints out the stations available to choose from.
   #
   def display_lines
-    @lines = Tube.new.lines.keys
-    @all_lines = @lines.join(", ")
-    @title_list_lines = @all_lines.split.map(&:capitalize).join(" ")
-    puts "\nYou can choose from the following lines: #{@title_list_lines}"
+    lines = Tube.new.lines.keys
+    all_lines = lines.join(", ")
+    title_list_lines = all_lines.split.map(&:capitalize).join(" ")
+    puts "\nYou can choose from the following lines: #{title_list_lines}"
   end
 
   def get_start_line
@@ -44,11 +44,11 @@ class Journey
   end
 
   def list_all_stations(tube_line)
-    @all_stations = Tube.new.lines[tube_line.to_sym] 
-    @list_of_stations = @all_stations.join(", ")
-    @print_list_of_stations = @list_of_stations.gsub("_", " ")
-    @title_list_of_stations = @print_list_of_stations.split.map(&:capitalize).join(" ")
-    puts "\nThese are the stations on that line: \n\n#{@title_list_of_stations}"
+    all_stations = Tube.new.lines[tube_line.to_sym] 
+    list_of_stations = all_stations.join(", ")
+    print_list_of_stations = list_of_stations.gsub("_", " ")
+    title_list_of_stations = print_list_of_stations.split.map(&:capitalize).join(" ")
+    puts "\nThese are the stations on that line: \n\n#{title_list_of_stations}"
   end
 
   #3. Add code to allow the user to select a starting station.
@@ -62,7 +62,7 @@ class Journey
   #
   def get_end_line
     puts "\nWhat tube line will you end on?"
-    @end_line = gets.strip.chomp.downcase
+    end_line = gets.strip.chomp.downcase
   end
 
   #5. Add code to prompt the user for an ending station.
@@ -78,39 +78,39 @@ class Journey
     #get start station, get end station, create an array with these stations and stations between, print length of array 
     #Using the start staion value I want to get the index of hash array
     #
-    @start_station_sym = @start_station.gsub(" ", "_").to_sym
+    start_station_sym = @start_station.gsub(" ", "_").to_sym
     #Using the end staion value I want to get the index of hash array
     #
-    @end_station_sym = @end_station.gsub(" ", "_").to_sym
+    end_station_sym = @end_station.gsub(" ", "_").to_sym
     #convert line to sym
     #
-    @start_line_sym = @start_line.to_sym
+    start_line_sym = @start_line.to_sym
     #Return an array for selected line:
     #
-    @two_d_array_of_stations_on_start_line = Tube.new.lines.values_at(@start_line_sym)
-    if @start_line_sym == :victoria
-      @start_line_sym_index = 0
+    two_d_array_of_stations_on_start_line = Tube.new.lines.values_at(start_line_sym)
+    if start_line_sym == :victoria
+      start_line_sym_index = 0
     end
-    @one_d_array_of_stations = @two_d_array_of_stations_on_start_line[@start_line_sym_index]
+    one_d_array_of_stations = two_d_array_of_stations_on_start_line[start_line_sym_index]
     #Use start station symbol to find index of station
     #
-    @start_index = @one_d_array_of_stations.find_index(@start_station_sym) 
+    start_index = one_d_array_of_stations.find_index(start_station_sym) 
     #Use end station symbol to find index of station
     #
-    @end_index = @one_d_array_of_stations.find_index(@end_station_sym)
+    end_index = one_d_array_of_stations.find_index(end_station_sym)
     #Use index of start and finish to calculate number of stops en route
     #
-    @number_of_stops = ((@end_index.to_i - @start_index.to_i) + 1)
+    number_of_stops = ((end_index.to_i - start_index.to_i) + 1)
 
-    puts "\nThere are #{@number_of_stops} stations till your final destination"
+    puts "\nThere are #{number_of_stops} stations till your final destination"
     #Use indexes to print stations between and including
     #
-    @stations_en_route = @one_d_array_of_stations.slice(@start_index..@end_index)
+    stations_en_route = one_d_array_of_stations.slice(start_index..end_index)
     #Generate list and make print print ready
     #
-    @print_stations_en_route = @stations_en_route.join(", ").gsub("_", " ").split.map(&:capitalize).join(" ")
+    print_stations_en_route = stations_en_route.join(", ").gsub("_", " ").split.map(&:capitalize).join(" ")
 
-    puts "\nThe stations en route are #{@print_stations_en_route}"
+    puts "\nThe stations en route are #{print_stations_en_route}"
     
   end
 
